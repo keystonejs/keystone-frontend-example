bd1c32166e3540b68af05c6b07fd7354e03e8e76
Noviny BC <noviny@thinkmill.com.au>
2022-02-11 19:47:01 +1100

    Pulling across changes from 'completed'

---
+++ src/components/index.tsx
@@ -0,0 +1,108 @@
+import React, { useMemo } from "react";
+import { Task } from "../types";
+
+const getPriorityColor = (priority: string) =>
+  priority === "low"
+    ? "mediumspringgreen"
+    : priority === "medium"
+    ? "orange"
+    : "red";
+
+const cardStyle = {
+  margin: "auto",
+  width: 360,
+  padding: 4,
+  borderRadius: 3,
+  marginTop: 4,
+};
+
+export const Divider = () => (
+  <div
+    style={{
+      width: 120,
+      height: 1,
+      marginLeft: "auto",
+      marginRight: "auto",
+      marginTop: 16,
+      marginBottom: 16,
+      background: "black",
+    }}
+  />
+);
+
+export const TodoItem = ({
+  label,
+  priority,
+  isComplete,
+  onCheckboxChange = () => {},
+}) => {
+  return (
+    <div style={cardStyle}>
+      <input
+        type="checkbox"
+        checked={isComplete}
+        onChange={() => {
+          onCheckboxChange();
+        }}
+      />
+      <div
+        style={{
+          height: 8,
+          width: 8,
+          borderRadius: 180,
+          background: getPriorityColor(priority),
+          display: "inline-block",
+          marginLeft: 8,
+          marginRight: 8,
+        }}
+      />
+      <span>{label}</span>
+    </div>
+  );
+};
+
+// Why are we doing this?
+// Well, KS lets you sort by priority, but you can only specify ASC or DESC
+// which is alphabetical.
+const priorityFunction = (a: Task, b: Task): -1 | 1 => {
+  const priorities = ["high", "medium", "low"];
+
+  return priorities.indexOf(a.priority) > priorities.indexOf(b.priority)
+    ? 1
+    : -1;
+};
+
+export const ShowTasks = ({
+  heading = "Still To Do 💻",
+  onCheckboxChange = () => {},
+  tasks,
+}: {
+  tasks: Task[];
+  onCheckboxChange?: (task: Task) => void;
+  heading: string;
+}) => {
+  const sortedTasks = useMemo(() => [...tasks].sort(priorityFunction), [tasks]);
+
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>{heading}</h2>
+      {sortedTasks.map((todoItem) => (
+        <TodoItem
+          key={todoItem.id}
+          {...todoItem}
+          onCheckboxChange={() => onCheckboxChange(todoItem)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export const Error = ({ error }) => (
+  <div>
+    <div>
+      We had an error contacting the Keystone server - make sure it's up and
+      running.
+    </div>
+    <div>Error message: {error.message}</div>
+  </div>
+);
